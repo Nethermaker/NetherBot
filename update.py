9f5f87dbd66643ccc59672970b1ad64a0d7817c7
Nethermaker <tsenne2@huskers.unl.edu>
2020-04-12 01:25:25 -0500

    Added time delay to update command

--- update.py
+++ update.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import time
 
 # There is likely a better way to do this but I couldn't find it
 
@@ -7,4 +8,5 @@ args = [arg for arg in sys.argv[1:] if not arg.startswith('-')]
 pid = args[0]
 os.system(f'kill {pid}')
 os.system(f'git pull')
+time.sleep(10)
 os.system(f'python3 main.py')
