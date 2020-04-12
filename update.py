42203bc96a5c8b18f16680a31a0d1febca539ef7
Nethermaker <tsenne2@huskers.unl.edu>
2020-04-12 01:17:35 -0500

    Finalize initial version of update.py

--- update.py
+++ update.py
@@ -1,5 +1,10 @@
 import sys
 import os
 
+# There is likely a better way to do this but I couldn't find it
+
 args = [arg for arg in sys.argv[1:] if not arg.startswith('-')]
-print(args[0])
+pid = args[0]
+os.system(f'kill {pid}')
+os.system(f'git pull')
+os.system(f'python3 main.py')
